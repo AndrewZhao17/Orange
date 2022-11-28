ba88e6bd4ae0401c2ae826a18525c76ae4df5362
AndrewZhao17 <117668420+AndrewZhao17@users.noreply.github.com>
2022-11-28 12:46:42 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function me(){
+  window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+}
+function dumb(){
+    document.addEventListener("keydown", (e)=>{
+   if (e.key == 'Enter'){
+     window.location.href = "error.html"
+   }
+    })
+}
+
+ document.addEventListener("keydown", d =>{
+    if (
+      d.key.toLowerCase() === "e"
+    && d.shiftKey
+      &&d.ctrlKey
+    ){
+      prompt("Good job finding this (all luck no skill) enter a code to access")
+    }
+ }
+  )
